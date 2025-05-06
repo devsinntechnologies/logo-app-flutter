a5fc5521a6046dcb66fa090facc06b22d6b2f267
Tahir Hassan <hmtahirhassan@gmail.com>
2025-05-06 04:22:10 -0700

    new changes

--- logo_app_flutter/lib/main.dart
+++ logo_app_flutter/lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Auto Desing Module',
+      title: 'Auto Design Module',
       home: const HomeScreen(),
     );
   }
