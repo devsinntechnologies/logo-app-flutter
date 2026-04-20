6ecf7bbb057a5142595acfffff1700602ace5b79
Arham Sarwar <arhamsarwar786@gmail.com>
2026-04-20 16:48:53 +0500

    Update Headers and Release New Build 1.0.0+4

--- lib/services/category_selection_service.dart
+++ lib/services/category_selection_service.dart
@@ -1,18 +1,21 @@
 import 'dart:convert';
+import 'dart:developer';
 import 'package:http/http.dart' as http;
 import 'package:logo_app_flutter/models/industry_model.dart';
 
 class CategorySelectionService {
-static Future<IndustryModel> getIndustries() async {
-  final response = await http.get(
-    Uri.parse('https://www.smart-logomaker.com/api/industries'),
-  );
+  static Future<IndustryModel> getIndustries() async {
+    final response = await http.get(
+        Uri.parse('https://www.smart-logomaker.com/api/industries'),
+        headers: {
+          'Content-Type': 'application/json',
+          'x-app-id': 'com.devsinntechnologies.smartlogomaker'
+        });
 
-  if (response.statusCode == 200) {
-    return IndustryModel.fromJson(jsonDecode(response.body));
-  } else {
-    throw Exception('Failed to load industries');
+    if (response.statusCode == 200) {
+      return IndustryModel.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to load industries');
+    }
   }
 }
-}
-
